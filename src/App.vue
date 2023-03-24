a0b9fc0a2a51cf544863cecec4134af35b6cf469
lxy <1406062098@qq.com>
2023-03-24 10:32:31 +0800

    feat: 设置全局动态的样式与设置兼容低版本的浏览器问题

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,3 @@
-<script setup lang="ts">
-import { Toast } from "vant";
-import { RouterLink, RouterView } from "vue-router";
-const aa = ()=>{
-  new Toast('提示内容')
-}
-aa()
-</script>
-
 <template>
   <div class="test">
     112121212
@@ -14,6 +5,15 @@ aa()
   <van-button type="primary" size="small">主要按钮</van-button>
   <!-- <RouterView /> -->
 </template>
+
+<script setup lang="ts">
+import { Toast } from "vant";
+import { RouterLink, RouterView } from "vue-router";
+const aa = ()=>{
+  Toast('提示内容')
+}
+aa()
+</script>
 
 <style scoped>
 .test{
--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,22 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
+import legacy from '@vitejs/plugin-legacy'
 
 import Components from 'unplugin-vue-components/vite';
 import { VantResolver } from 'unplugin-vue-components/resolvers';
-
+console.log(defineConfig,'555')
 // https://vitejs.dev/config/
 export default defineConfig({
+  
   plugins: [
     vue(),
     vueJsx(),
     Components({
       resolvers: [VantResolver()],
+    }),
+    legacy({
+      targets: ['defaults', 'not IE 11'],
     }),
   ],
   resolve: {
@@ -21,4 +26,13 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  css:{
+    // 动态设置全局样式
+    preprocessorOptions:{
+      less:{
+        charset:false,
+        additionalData:`@import "./src/css/global.less";`
+      }
+    }
+  }
 });
